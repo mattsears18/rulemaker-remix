a2ae60b096412a2043f874f72ab4362a8d490e15
Matt Sears <mattsears18@gmail.com>
2022-08-28 14:17:43 -0400

    fix cypress test

--- cypress/e2e/smoke.cy.ts
+++ cypress/e2e/smoke.cy.ts
@@ -19,12 +19,12 @@ describe('smoke tests', () => {
     cy.findByLabelText(/password/i).type(loginForm.password)
     cy.findByRole('button', { name: /create account/i }).click()
 
-    cy.findByRole('link', { name: /notes/i }).click()
+    cy.findByRole('link', { name: /proposals/i }).click()
     cy.findByRole('button', { name: /logout/i }).click()
     cy.findByRole('link', { name: /log in/i })
   })
 
-  it('should allow you to make a note', () => {
+  it('should allow you to make a proposal', () => {
     const testNote = {
       title: faker.lorem.words(1),
       body: faker.lorem.sentences(1),
@@ -32,10 +32,10 @@ describe('smoke tests', () => {
     cy.login()
     cy.visitAndCheck('/')
 
-    cy.findByRole('link', { name: /notes/i }).click()
-    cy.findByText('No notes yet')
+    cy.findByRole('link', { name: /proposals/i }).click()
+    cy.findByText('No proposals yet')
 
-    cy.findByRole('link', { name: /\+ new note/i }).click()
+    cy.findByRole('link', { name: /\+ new proposal/i }).click()
 
     cy.findByRole('textbox', { name: /title/i }).type(testNote.title)
     cy.findByRole('textbox', { name: /body/i }).type(testNote.body)
@@ -43,6 +43,6 @@ describe('smoke tests', () => {
 
     cy.findByRole('button', { name: /delete/i }).click()
 
-    cy.findByText('No notes yet')
+    cy.findByText('No proposals yet')
   })
 })
